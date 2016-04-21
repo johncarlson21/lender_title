80bbf9532679a645ca6556b9244fc58eef3ccda9
John Carlson <johncarlson21@gmail.com>
2016-04-21 00:05:56 -0500

    Add AdminLTE Template engine

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,25 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+@section('htmlheader_title')
+	Home
+@endsection
 
-                <div class="panel-body">
-                    You are logged in!
-                    @role('admin')
-                    <div class="alert alert-success">You are an admin</div>
-                    @endrole
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
+@section('main-content')
+	<div class="container spark-screen">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="panel panel-default">
+					<div class="panel-heading">Home</div>
+
+					<div class="panel-body">
+						You are logged in!
+@role('admin')
+<div class="alert alert-success">You are an admin</div>
+@endrole
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
